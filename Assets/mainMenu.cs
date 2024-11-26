5478e08b46104fc6f55c9b3039e2e79a91cd4ec7
Christopher Friedrichs <chfr1294@stud.hs-flensburg.de>
2024-11-26 15:37:37 +0100

    Canva so gut wie fertig nur noch controller mappen und quit btn einfügen

---
+++ Assets/mainMenu.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System;
+
+public class mainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadSceneAsync(1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    Console.WriteLine("Beendet");
+    }
+}
